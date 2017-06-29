6a9d9f8e5f205d332821d0fda5dc809d5e5044cb
Edward <elin@paradymemanagement.com>
2017-06-29 17:55:45 -0400

    Merged changes

--- ParadymeIntern/RSR/filters.py
+++ ParadymeIntern/RSR/filters.py
@@ -2,20 +2,7 @@ import django_filters
 from .models import Person
 
 class PersonFilter(django_filters.FilterSet):
-    school = django_filters.ModelChoiceFilter(name='school', queryset=Person.objects.order_by().
-                                              values_list('school', flat=True).distinct())
-    school_level = django_filters.ModelChoiceFilter(name='school_level', queryset=Person.objects.order_by().
-                                                    values_list('school_level', flat=True).distinct())
-    major = django_filters.ModelChoiceFilter(name='major', queryset=Person.objects.order_by().
-                                             values_list('major', flat=True).distinct())
-    work_authorization = django_filters.ModelChoiceFilter(name='work_authorization', queryset=Person.objects.order_by().
-                                                          values_list('work_authorization', flat=True).distinct())
-    security_clearance = django_filters.ModelChoiceFilter(name='security_clearance', queryset=Person.objects.order_by().
-                                                          values_list('security_clearance', flat=True).distinct())
-    graduation_year = django_filters.ModelChoiceFilter(name='graduation_year', queryset=Person.objects.order_by().
-                                                       values_list('graduation_year', flat=True).distinct())
-    graduation_month = django_filters.ModelChoiceFilter(name='graduation_month', queryset=Person.objects.order_by().
-                                                        values_list('graduation_month', flat=True).distinct())
+
     language = django_filters.CharFilter(name='language', lookup_expr='icontains')
     skills = django_filters.CharFilter(name='skills', lookup_expr='icontains')
     certificate = django_filters.CharFilter(name='certificate', lookup_expr='icontains')
