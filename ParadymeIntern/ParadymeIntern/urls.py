3d4be335c1f258f8a157351b8b419ab122d0bfef
intherenzone <maviles@umd.edu>
2017-07-07 15:39:56 -0400

    merged new models, website up and running

--- ParadymeIntern/ParadymeIntern/urls.py
+++ ParadymeIntern/ParadymeIntern/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from django.contrib.auth import views
+from RSR.views import *
 
 
 urlpatterns = [
